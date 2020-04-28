f8f3e466d049cdbad2652273e5041be1c18c4d18
temlerg <56558240+temlerg@users.noreply.github.com>
2020-04-28 21:03:05 +0300

    Initial commit

---
+++ tey.py
@@ -0,0 +1,9 @@
+print("Введите длины сторон предполагаемого треугольника:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+if a + b > c and a + c > b and b + c > a:
+    print("Треугольник существует")
+else:
+    print("Треугольник не существует")
